dd0cd5850ec489c43e2066711208bb5a9253402f
wermos <63574588+wermos@users.noreply.github.com>
2025-01-16 00:16:22 +0530

    Removed a commented snippet.

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,4 @@ fn main() {
     b.set(2, 4); // Square C5
 
     println!("{}", b);
-
-    // let mut ans: u64 = 1 << 32;
-    // let mask = 1 << 5;
-
-    // ans |= mask;
-
-    // let occ = (ans >> 5) & 1;
-
-    // println!("Number: {:b}\t Mask: {:b}\nOccupancy: {}", ans, mask, occ);
 }
