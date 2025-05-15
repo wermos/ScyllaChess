f79cfc2cdc5061ae53efecd1a006cf9dc02c6920
wermos <63574588+wermos@users.noreply.github.com>
2025-05-15 13:15:16 +0530

    Minor sanity test.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,15 @@
 
 #include "bitboard.hpp"
 
+using namespace scy;
+
 int main() {
-    return 0;
+    Square e4 = "e4";    // From std::string
+    Square d5 = 28;      // From integer index
+    std::string notation = d5;  // To std::string
+    int index = e4;             // To integral index
+    
+    // Literal syntax
+    auto a1 = "a1"_sq;
+    uint64_t board = 1ULL << static_cast<int>(a1);
 }
