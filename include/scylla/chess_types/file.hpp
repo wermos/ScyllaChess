b143bc5296fd705f66f3d9c98c6ebcb8c3ea3875
wermos <63574588+wermos@users.noreply.github.com>
2025-05-19 12:17:50 +0530

    Modified the definition of the `File` class.

--- include/scylla/chess_types/file.hpp
+++ include/scylla/chess_types/file.hpp
@@ -8,51 +8,66 @@
 namespace scy {
 
 class File {
-public:
-    // explicit constexpr File(std::uint8_t f) : file(f) {
-    //     if (f > 7) throw std::invalid_argument("Invalid file");
-    // }
-
-    explicit constexpr File(char c) :
-        file((c >= 'a' && c <= 'h') ? c - 'a' : throw std::invalid_argument("Invalid file")) {}
+   public:
+    explicit constexpr File(char c)
+        : file((c >= 'a' && c <= 'h')
+                   ? c - 'a'
+                   : throw std::invalid_argument("Invalid file")) {}
 
     /// TODO: these might not be needed
-    explicit constexpr File(const std::string& notation) : file(validate(notation)) {}
-    explicit constexpr File(const char* notation) : File(std::string{notation}) {}
+    explicit constexpr File(const std::string& notation)
+        : file(validate(notation)) {}
+    explicit constexpr File(const char* notation)
+        : File(std::string{notation}) {}
 
     // Predefined files
-    static constexpr File A() noexcept { return File('a'); }
-    static constexpr File B() noexcept { return File('b'); }
-    static constexpr File C() noexcept { return File('c'); }
-    static constexpr File D() noexcept { return File('d'); }
-    static constexpr File E() noexcept { return File('e'); }
-    static constexpr File F() noexcept { return File('f'); }
-    static constexpr File G() noexcept { return File('g'); }
-    static constexpr File H() noexcept { return File('h'); }
+    static constexpr File A() noexcept {
+        return File('a');
+    }
+    static constexpr File B() noexcept {
+        return File('b');
+    }
+    static constexpr File C() noexcept {
+        return File('c');
+    }
+    static constexpr File D() noexcept {
+        return File('d');
+    }
+    static constexpr File E() noexcept {
+        return File('e');
+    }
+    static constexpr File F() noexcept {
+        return File('f');
+    }
+    static constexpr File G() noexcept {
+        return File('g');
+    }
+    static constexpr File H() noexcept {
+        return File('h');
+    }
 
-    /// we prefer to overload the std::string operator and the operator<<
-    /// (stream) operator instead of this nonsense.
-    // // Conversion
-    // static constexpr File from_char(char c) {
-    //     if(c < 'a' || c > 'h') throw std::invalid_argument("Invalid file");
-    //     return File(c - 'a');
+    /// having both std::uint8_t and std::size_t overloads causes ambiguity in
+    /// the Bitboard constructor
+    // constexpr operator std::size_t() const noexcept {
+    //     return file;
     // }
-    
-    // constexpr char to_char() const noexcept { return 'a' + file; }
-    
-    constexpr std::uint8_t index() const noexcept {
-        /// TODO: is this function really needed?
+
+    constexpr operator std::uint8_t() const noexcept {
         return file;
     }
-    
-    // Arithmetic
-    constexpr File next() const noexcept { return File((file + 1) % 8); }
-    constexpr File previous() const noexcept { return File((file - 1) % 8); }
-    
+
+    // Iterators
+    constexpr File next() const noexcept {
+        return File((file + 1) % 8);
+    }
+    constexpr File previous() const noexcept {
+        return File((file - 1) % 8);
+    }
+
     // Comparison
     auto operator<=>(const File&) const = default;
-    
-private:
+
+   private:
     constexpr static std::size_t validate(const std::string& s) {
         if (s.length() != 1) {
             throw std::invalid_argument("Invalid file");
@@ -67,7 +82,9 @@ private:
         return file - 'a';
     }
 
-    std::uint8_t file; // 0-7 (a-h)
+    std::uint8_t file;
+    // internally stores 0-7,
+    // takes user input between a-h
 };
 
-}
+}  // namespace scy
