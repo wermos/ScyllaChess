ad3618bca1846d866b467d109947f00ef60016fe
wermos <63574588+wermos@users.noreply.github.com>
2025-05-23 00:35:12 +0530

    Removed the commented code.

--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,6 @@
 using namespace scy;
 
 int main() {
-    // Square e4 = "e4";           // From std::string
-    // Square d5 = 28;             // From integer index
-    // std::string notation = d5;  // To std::string
-    // int index = e4;             // To integral index
-
-    // // Literal syntax
-    // auto a1 = "a1"_sq;
-
-    // Bitboard board;
-    // std::cout << "Initial : " << std::endl;
-    // std::cout << board << std::endl;
-
-    // auto d7 = "d7"_sq;
-
-    // board.set(a1);
-    // board.set(d7);
-    // board.set(63);
-    // board.set("e6");
-    // board.set("c4"_sq);
-
-    // std::cout << "After setting some: " << std::endl;
-    // std::cout << board << std::endl;
-
-    // board.unset(d7);
-    // board.unset(63);
-    // board.unset("e6");
-    // board.unset("c4"_sq);
-    // board.unset("c8");  // Not set, should not change anything
-
-    // std::cout << "After unsetting some: " << std::endl;
-    // std::cout << board << std::endl;
     Bitboard b;
 
     std::cout << "Initial state:\n" << b << std::endl;
